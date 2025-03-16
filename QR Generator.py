37821ac72ce0d960b6c7016826becfe4980bada7
Arnob <arnobg2002@gmail.com>
2025-03-16 01:32:34 -0230

    using matplotlib for rough visualisation
    we can decide to shift to some other visualising library later. I just find matplotlib convenient.

--- QR Generator.py
+++ QR Generator.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 def converter(string):
     binary_array=[]
     for char in string:
@@ -33,3 +35,22 @@ print(qr_mode(mode))
 print(qr_character_count(string))
 binary_list = converter(string)
 mode = "alphanumeric"
+
+
+G = np.zeros((25,25,3))
+
+def draw_square(starting_pos, length, color):
+    for i in range(length):
+        if(starting_pos[1]>=0):
+            G[starting_pos[0]+i] [starting_pos[1]] = color
+        if(starting_pos[1]+length<=25):
+            G[starting_pos[0]+i] [starting_pos[1]+length-1] = color
+        if(starting_pos[0]>=0):
+            G[starting_pos[0]] [starting_pos[1]+i] = color
+        if(starting_pos[0]+length<=25):
+            G[starting_pos[0]+length-1] [starting_pos[1]+i] = color
+
+draw_square((1,1), 5, [1, 1, 1])
+draw_square((1,19), 5, [1, 1, 1])
+plt.matshow(G)
+plt.show()
